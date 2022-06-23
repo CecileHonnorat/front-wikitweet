fb6c71c3aa011bbe608613b9be599b7377e17b82
CécileH <97221105+CecileHonnorat@users.noreply.github.com>
2022-06-23 13:54:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+import Tweet from './components/tweet'
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Modal } from 'antd';
 
-function App() {
+export default function App() {
+
+  const [tweets, setTweets] = useState([])
+
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+
+  const [tweetName, setTweetName] = useState('');
+  const [tweetContent, setTweetContent] = useState('');
+
+  const [searchAuthor, setSearchAuthor] = useState("")
+
+console.log(searchAuthor)
+  // récupérer les tweets de la bdd au chargement de la page
+  useEffect(() => {
+    // Récupérer les tweets d'un auteur spécifique : 
+    if (searchAuthor !== ""){
+      const loadData = async () => {
+        const rawData = await fetch(`/tweets/${searchAuthor}`);
+        const data = await rawData.json();
+        setTweets(data.authorTweets)
+        console.log(data)
+      }
+      loadData()
+    } else {
+      const loadData = async () => {
+        const rawData = await fetch('/alltweets');
+        const data = await rawData.json();
+        setTweets(data.allTweets)
+        console.log(data)
+      }
+      loadData()
+    }
+  }, [confirmLoading, searchAuthor])
+
+  console.log(tweets)
+
+  let tweetList = tweets.map((t, i) => {
+      return (
+        <Tweet key={t._id} author={t.authorName} content={t.content} date={t.date} />
+      )
+    }).reverse()
+
+  // Gestion de la modal
+  const showModal = () => {
+    setVisible(true);
+    setTweetName('');
+    setTweetContent('');
+  };
+
+  const handleOk = () => {
+    setConfirmLoading(true);
+    sendTweet()
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+    setTweetName();
+    setTweetContent();
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+    setTweetName();
+    setTweetContent();
+  };
+
+  // Envoi d'un nouveau Tweet :
+  const sendTweet = async () => {
+    const tweetData = await fetch('/sendtweet', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: `authorName=${tweetName}&content=${tweetContent}`
+    })
+
+    const body = await tweetData.json()
+
+    console.log(body)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to WikiTweet !</h1>
+      <div>
+        <input placeholder="Chercher en auteur" className='search'
+         onChange={(e) => setSearchAuthor(e.target.value)}/>
+      </div>
+      <button className='envoyerTweet' onClick={() => showModal()}>Envoyer un Tweet</button>
+      <Modal
+        title='Ecrivez votre tweet !'
+        visible={visible}
+        onOk={handleOk}
+        okText="Envoyer"
+        cancelText="Annuler"
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <form className='formTweet'>
+          <input type="text" placeholder='Nom' className='nameInput'
+            onChange={(e) => setTweetName(e.target.value)} />
+          <textarea type="text" placeholder='Ajouter un tweet' className='tweetInput'
+            onChange={(e) => setTweetContent(e.target.value)} />
+        </form>
+      </Modal>
+      {tweetList}
     </div>
   );
 }
 
-export default App;
